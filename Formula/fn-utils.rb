241f6dbb50949cf9be062c9ae28c09fc92f79da2
Ben "Moose" Hadfield <16048190+benmoose@users.noreply.github.com>
2026-09-01 13:01:26 +0100

    Update fn-utils.rb

--- Formula/fn-utils.rb
+++ Formula/fn-utils.rb
@@ -1,48 +1,42 @@
 class FnUtils < Formula
-  desc "My custom CLI utility functions"
+  desc "Collection of useful Zsh CLI functions"
   homepage "https://github.com/benmoose/homebrew-cli-utils"
-  url "https://github.com/benmoose/homebrew-cli-utils/archive/refs/tags/v0.0.53.tar.gz"
-  sha256 "d0439108f35d09e7420fa3f593cd688750658cdbc8149ee4c7e001dde6b24571"
-  license "GPL-3.0"
+  url "https://github.com/benmoose/homebrew-cli-utils/archive/refs/tags/v0.0.111.tar.gz"
+  sha256 "588410971dbfff861590beee18d93178cd4a77dc34083a548d80d2ce6e327672"
+  license "GPL-3.0-or-later"
+  head "https://github.com/benmoose/homebrew-cli-utils.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+){2})$/i)
+  end
 
   def install
     prefix.install_metafiles
 
-    zsh_function.install Dir["src/private/*"]
-    zsh_function.install Dir["src/public/*"]
-    
-    inreplace "src/fn-utils.sh", "$1", "#{zsh_function}"
-    bin.install "src/fn-utils.sh" => "fn-utils"
+    zsh_function.install Pathname.glob("functions/**/*.zsh").to_h { |path| [path, path.basename.sub_ext('')] }
+
+    pkgshare.install Dir["src/*"]
+    prefix.install_symlink pkgshare/"init.zsh" => "init"
+    bin.install_symlink pkgshare/"install.zsh" => installer_name
   end
 
-  def post_install
-    system "fn-utils"
-  end
-
-  def caveat
+  def caveats
     <<~EOS
-      fn-utils installed!
-      
-      To use add this to your .zshrc:
-        `source <(fn-utils)`
+      To autoload functions, add this to your profile:
+        source #{opt_prefix}/init
 
+      or run "#{installer_name}" to configure .zshrc automatically.
     EOS
   end
 
-  # test do
-  # `test do` will create, run in and delete a temporary directory.
-  #
-  # This test will fail and we won't accept that! For Homebrew/homebrew-core
-  # this will need to be a test that verifies the functionality of the
-  # software. Run the test with `brew test fn-utils`. Options passed
-  # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-  #
-  # The installed folder is not in the path, so use the entire path to any
-  # executables being tested: `system bin/"program", "do", "something"`.
-  # system "false"
-  # end
+  test do
+    expect(formula.pkgshare).to be_a_directory
+  end
 
-  # def fn_names
-  #   Dir[zsh_function/"*"].map {|fn| File.basename(fn)}
-  # end
+  private
+
+  def installer_name
+    "install-#{name}"
+  end
 end
