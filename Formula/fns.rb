980bf119cae1da84dcf25709fd99563e6a618e31
Ben "Moose" Hadfield <16048190+benmoose@users.noreply.github.com>
2026-09-07 15:33:39 +0100

    symlink init script (#7)

--- Formula/fns.rb
+++ Formula/fns.rb
@@ -13,13 +13,11 @@ class Fns < Formula
 
   def install
     prefix.install_metafiles
-    pkgshare.install "src/fns/env.zsh", "src/fns/init.zsh"
-    pkgshare.install "src/fns/data"
 
-    zsh_function.install Dir["src/fns/functions/*.zsh"].to_h { |path|
-      [path, path.delete_suffix(".zsh").split("/").last]
-    }
-    zsh_function.install Dir["src/fns/functions/private/*.zsh"].to_h { |path|
+    pkgshare.install "src/fns/env.zsh", "src/fns/init.zsh", "src/fns/data"
+    prefix.install_symlink pkgshare/"init.zsh" => "#{name}-init"
+
+    zsh_function.install Dir["src/fns/functions/*.zsh", "src/fns/functions/private/*.zsh"].to_h { |path|
       [path, path.delete_suffix(".zsh").split("/").last]
     }
   end
@@ -27,7 +25,7 @@ class Fns < Formula
   def caveats
     <<~EOS
       To autoload functions, add this to your profile:
-        source #{opt_prefix}/#{name}.zsh
+        source #{opt_prefix}/#{name}-init
     EOS
   end
 
--- src/fns/init.zsh
+++ src/fns/init.zsh
@@ -7,11 +7,14 @@ emulate -L zsh
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 
 _init() {
-	local -r func_dir="${1:a:h:h}/zsh/site-functions"
+	local -r \
+		name="${1:t}" \
+		func_dir="${1:a:h}/share/zsh/site-functions"
+
 	if ! [[ -d "$func_dir" ]]; then
 		builtin printf \
 			"%s: installed functions not found, expect directory at %s.\nTry running '%q'\n" \
-			"init" "$func_dir" "brew reinstall fns" >&2
+			"$name" "$func_dir" "brew reinstall fns" >&2
 		return 1
 	fi
 
@@ -25,7 +28,7 @@ _init() {
 {
 	[[ "${zsh_eval_context[-1]}" == "file" ]] || return 1
 
-	source "${0:a:h}/env.zsh"
+	source "${0:h}/share/fns/env.zsh"
 	_init "$0"
 } always {
 		unset -f _init
