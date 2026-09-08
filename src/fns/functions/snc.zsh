fae6e6dc347acc4313260cedad39843f604c9143
Ben Hadfield <16048190+benmoose@users.noreply.github.com>
2026-09-08 12:23:02 +0100

    fix snc

--- src/fns/functions/snc.zsh
+++ src/fns/functions/snc.zsh
@@ -6,12 +6,13 @@ set -o pipefail
 if ! _git_repo; then return 1; fi
 
 setopt no_monitor
-() {
-	git fetch --no-auto-gc --no-tags --quiet origin || return $?
-} &
+
+git fetch --no-auto-gc --no-tags --quiet origin &
 local -r pid="$!"
+trap 'kill "$pid"; rm -f "$_err"; return 130' INT TERM
 
 _spinner "$pid" "Comparing upstream"
+wait "$pid" 2>/dev/null
 
 local -r \
 	curr_b="$(_git_current_branch)" \
@@ -19,9 +20,9 @@ local -r \
 	upstream_obj="$(git rev-parse --short --verify -q HEAD@{upstream})"
 
 if [[ "$head_obj" != "$upstream_obj" ]]; then
-	[[ "$1" == "-q" ]] || _err "✕ $curr_b diverges from upstream, $upstream_obj"
+	[[ "$1" == "-q" ]] || printf "${RED}${BOLD}✕${NS} $curr_b diverges from upstream branch\n"
 	return 1
 fi
 
-[[ "$1" == "-q" ]] || printf "$GREEN✓$NS $curr_b branch is up to date\n"
+[[ "$1" == "-q" ]] || printf "${GREEN}${BOLD}✓$NS $curr_b is up to date\n"
 return 0
