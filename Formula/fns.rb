94e598c48abeb780e11b87d9f2c80e9a8cd2201e
Ben "Moose" Hadfield <16048190+benmoose@users.noreply.github.com>
2026-09-08 11:05:24 +0000

    fns 0.0.8

--- Formula/fns.rb
+++ Formula/fns.rb
@@ -1,8 +1,8 @@
 class Fns < Formula
   desc "Collection of useful Zsh functions"
   homepage "https://github.com/benmoose/homebrew-tap"
-  url "https://github.com/benmoose/homebrew-tap/archive/refs/tags/v0.0.7.tar.gz"
-  sha256 "e379678ec3e7cc7bb8bc3f0ed870d7e8795da5950ab3657d2e50ac2962447904"
+  url "https://github.com/benmoose/homebrew-tap/archive/refs/tags/v0.0.8.tar.gz"
+  sha256 "f4d2df1270d669f97da14b66c80da97152b7421ba68a3222d95d5383e7f20ed0"
   license "GPL-3.0-or-later"
   head "https://github.com/benmoose/homebrew-tap.git", branch: "main"
 
