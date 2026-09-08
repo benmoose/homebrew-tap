d35db76f056edcfbf9ef8f1bae4b0495e013c992
Ben "Moose" Hadfield <16048190+benmoose@users.noreply.github.com>
2026-09-08 11:20:44 +0100

    spinner dots animate (#17)

--- src/fns/functions/private/_spinner.zsh
+++ src/fns/functions/private/_spinner.zsh
@@ -13,14 +13,24 @@ local -ar spinners=( $(<"${data_spinners:a}") )
 local -ir spinner_i=$(( ${SPINNER:-RANDOM} ))
 local -ar frames=( "${(ws::)spinners[$(( spinner_i % $#spinners[@] + 1 ))]}" )
 local -r pid_arg="$1" msg_arg="${2-Thinking}" colour_arg="${3-${SPINNER_COLOUR:-$CYAN}}"
+
+local GREY="$(\tput setaf 8)"
+local -ar dots=(
+	"${GREY}${DIM}...$NS" "${GREY}${DIM}...$NS" "${GREY}${DIM}...$NS"
+	"${GREY}${DIM}...$NS" "${GREY}${DIM}...$NS" "${GREY}${DIM}...$NS"
+	"$GREY.$DIM..$NS" "${GREY}${DIM}.${NS}${GREY}.$DIM.$NS" "${GREY}${DIM}..${NS}${GREY}.$NS"
+)
+
 local -i t
 
 {
 	\tput civis
 	while \kill -0 "$pid_arg" &> /dev/null; do
-		builtin printf "${CR}${BOLD}%s%s$NS %s$DIM...$NS " \
-			"$colour_arg" "${frames[$(( t++ % $#frames[@] + 1 ))]}" "$msg_arg"
-		\sleep 0.042
+		builtin printf "${CR}${BOLD}%s%s$NS %s%s " \
+			"$colour_arg" "${frames[$(( t % $#frames[@] + 1 ))]}" \
+			"$msg_arg" "${dots[$(( t/3 % $#dots[@] + 1 ))]}"
+		\sleep 0.044
+		((t++))
 	done
 } always {
 	\tput cnorm
